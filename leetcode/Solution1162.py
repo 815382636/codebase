54f1ba77686f06024546f23871a37b79a2aa6260
815382636 <815382636@qq.com>
2020-11-03 15:31:57 +0800

    leetcode 1162

--- leetcode/Solution1162.py
+++ leetcode/Solution1162.py
@@ -3,44 +3,65 @@ from typing import List
 
 class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
-        s = set()
-        for i in grid:
-            for j in i:
-                s.add(j)
-        if len(s) == 1:
-            return -1
-        self.grid = grid
-        self.dx = [1, -1, 0, 0]
-        self.dy = [0, 0, 1, -1]
-        self.row, self.col = len(grid), len(grid[0])
-        self.result = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] == 1:
-                    self.caculate(i, j, 0)
-        return self.result
+        n, m = len(grid), len(grid[0])
+        dis = [[float("inf") for j in range(m+2)] for i in range(n+2)]
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if grid[i-1][j-1]:
+                    dis[i][j] = 0
+                else:
+                    dis[i][j] = min(dis[i-1][j], dis[i][j-1]) + 1
+        res = -1
+        for i in range(n, 0, -1):
+            for j in range(m, 0, -1):
+                if grid[i-1][j-1]:
+                    dis[i][j] = 0
+                else:
+                    dis[i][j] = min(dis[i+1][j]+1, dis[i][j+1]+1, dis[i][j])
+                    res = max(dis[i][j], res)
+        return res if res != -1 and res != float("inf") else -1
 
-    def caculate(self, i, j, distance):
-        sign = False
-        sign_list = [False, False, False, False]
-        for k in range(4):
-            if 0 <= self.dx[k] + i < self.row and 0 <= self.dy[k] + j < self.col and not self.grid[self.dx[k] + i][
-                self.dy[k] + j]:
-                self.grid[self.dx[k] + i][self.dy[k] + j] = 1
-                sign = True
-                sign_list[k] = True
-        if sign:
-            distance += 1
-            for k in range(4):
-                if 0 <= self.dx[k] + i < self.row and 0 <= self.dy[k] + j < self.col:
-                    self.caculate(self.dx[k] + i, self.dy[k] + j, distance)
-            for k in range(4):
-                if sign_list[k]:
-                    self.grid[self.dx[k] + i][self.dy[k] + j] = 0
-        else:
-            self.result = self.result if self.result >= distance else distance
+    # BFS ,可惜可惜，python过不了
+    # def maxDistance(self, grid: List[List[int]]) -> int:
+    #     s = set()
+    #     for i in grid:
+    #         for j in i:
+    #             s.add(j)
+    #     if len(s) == 1:
+    #         return -1
+    #     dx = [1, -1, 0, 0]
+    #     dy = [0, 0, 1, -1]
+    #     row, col = len(grid), len(grid[0])
+    #     result = 0
+    #     for i in range(len(grid)):
+    #         for j in range(len(grid[i])):
+    #             if grid[i][j] == 0:
+    #                 mapp = [[0] * col for i in range(row)]
+    #                 mapp[i][j] = 1
+    #                 queue = [(i, j)]
+    #                 mid_result = 0
+    #                 distance = 0
+    #                 while queue and not mid_result:
+    #                     for ci in range(len(queue)):
+    #                         _ci = queue.pop(0)
+    #                         x = _ci[0]
+    #                         y = _ci[1]
+    #                         for k in range(4):
+    #                             if 0 <= x + dx[k] < row and 0 <= y + dy[k] < col:
+    #                                 if grid[x + dx[k]][y + dy[k]] == 1:
+    #                                     mid_result = distance + 1
+    #                                     break
+    #                                 if not mapp[x + dx[k]][y + dy[k]]:
+    #                                     mapp[x + dx[k]][y + dy[k]] = 1
+    #                                     queue.append((x + dx[k], y + dy[k]))
+    #                         if mid_result:
+    #                             break
+    #                     distance += 1
+    #                 result = result if result >= mid_result else mid_result
+    #     return result
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.maxDistance([[1, 0, 1], [0, 0, 0], [1, 0, 1]]))
+    # print(s.maxDistance([[1, 0, 0], [0, 0, 0], [0, 0, 0]]))
+    print(s.maxDistance([[0, 0, 1, 1, 1], [0, 1, 1, 0, 0], [0, 0, 1, 1, 0], [1, 0, 0, 0, 0], [1, 1, 0, 0, 1]]))
