6bbb683f6687428835a2b67973434e174b5b03da
815382636 <815382636@qq.com>
2020-11-22 12:40:56 +0800

    leetcode 57new

--- leetcode/Solution57.py
+++ leetcode/Solution57.py
@@ -3,16 +3,34 @@ from typing import List
 
 class Solution:
 
+    # def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+    #     intervals.append(newInterval)
+    #     intervals, index = sorted(intervals), 1
+    #     while index < len(intervals):
+    #         if intervals[index - 1][1] >= intervals[index][0]:
+    #             intervals[index - 1][1] = max(intervals[index - 1][1], intervals[index][1])
+    #             intervals.pop(index)
+    #         else:
+    #             index += 1
+    #     return intervals
+
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        intervals.append(newInterval)
-        intervals, index = sorted(intervals), 1
-        while index < len(intervals):
-            if intervals[index - 1][1] >= intervals[index][0]:
-                intervals[index - 1][1] = max(intervals[index - 1][1], intervals[index][1])
-                intervals.pop(index)
+        left, right = newInterval
+        ans, placed = [], False
+        for i, v in enumerate(intervals):
+            if v[0] > right:
+                if not placed:
+                    ans.append([left, right])
+                    placed = True
+                ans.append(v)
+            elif v[1] < left:
+                ans.append(v)
             else:
-                index += 1
-        return intervals
+                left = min(left, v[0])
+                right = max(right, v[1])
+        if not placed:
+            ans.append([left, right])
+        return ans
 
 
 if __name__ == "__main__":
