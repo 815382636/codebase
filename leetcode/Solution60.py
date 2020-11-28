901978b5b804f7888d74175a79089e4dcfc2bac1
815382636 <815382636@qq.com>
2020-11-28 09:42:18 +0800

    leetcode 60

--- leetcode/Solution60.py
+++ leetcode/Solution60.py
@@ -1,29 +1,23 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        l = [i for i in range(n)]
-        sign = [False] * n
-        self.k, self.result = k, None
-
-        def operate(s):
-            if self.k > 0:
-                if len(s) == n:
-                    self.k -= 1
-                    if self.k == 0:
-                        self.result = s
-                    return
-                for i in range(n):
-                    if not sign[i]:
-                        sign[i] = True
-                        operate(s + str(i + 1))
-                        if self.k == 0:
-                            return
-                        sign[i] = False
-        operate('')
-        return self.result
-
+        if n == k == 1:
+            return '1'
+        l = [i + 1 for i in range(n)]
+        factorial_list, result = [1], ''
+        for i in range(2, n):
+            factorial_list.append(factorial_list[-1] * i)
+        for i in range(len(factorial_list) - 1, -1, -1):
+            index = (k + factorial_list[i] - 1) // factorial_list[i]
+            k = k % factorial_list[i]
+            result += str(l[index - 1])
+            l.remove(l[index - 1])
+        result += str(l[0])
+        return result
 
 
 if __name__ == '__main__':
     s = Solution()
     print(s.getPermutation(3,
                            3))
+    print(s.getPermutation(1,
+                           1))
