4f4e2b6658607d912f99707920a0bdc53857f7d6
815382636 <815382636@qq.com>
2020-12-09 14:31:27 +0800

    设计模式： 单例模式、适配器模式

--- db_operation/mysql_connection.py
+++ db_operation/mysql_connection.py
@@ -1,15 +1,10 @@
-import MySQLdb
-
-conn = MySQLdb.connect(
-    host='localhost',
-    user='root',
-    passwd='weiqi19960906',
-    db='person',
-    port=3306,
-    charset='utf8'
-)
-
-cursor = conn.cursor()
-cursor.execute('select * from "person";')
-rest = cursor.fetchone()
-print(rest)
+import pymysql
+"""
+    python 连接mysql 教程
+    https://www.runoob.com/python3/python3-mysql.html
+"""
+db = pymysql.connect('localhost', 'root', 'weiqi19960906', 'mysqltest')
+cur = db.cursor()
+cur.execute("select * from user;")
+data = cur.fetchall()
+print(data)
