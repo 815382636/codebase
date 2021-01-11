db7d7e7f52357f361371b4bf8015fbe713702a2d
815382636 <815382636@qq.com>
2021-01-11 09:56:15 +0800

    leetcode 136 异或操作

---
+++ leetcode/Solution136.py
@@ -0,0 +1,10 @@
+"""
+    异或操作
+"""
+from typing import List
+from functools import reduce
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return reduce(lambda x, y: x ^ y, nums)
