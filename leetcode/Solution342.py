c64c54249edf86d6fb13581ff5ef32457dbd8104
815382636 <815382636@qq.com>
2021-01-26 13:09:45 +0800

    leetcode 342,344

--- leetcode/Solution342.py
+++ leetcode/Solution342.py
@@ -1,3 +1,8 @@
+"""
+    2的幂写成二进制是0b10000，一个1后边根几个0
+    2的幂-1就会是0b1111
+    0xaaaaaaaa ---> 8个1010
+"""
 class Solution:
     def isPowerOfFour(self, num: int) -> bool:
         return num > 0 and num & (num - 1) == 0 and num & 0xaaaaaaaa == 0
